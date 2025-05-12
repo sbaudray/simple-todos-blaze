48bdb4cbff6bddea54a790840f51ae004de03991
sbaudray <22913636+sbaudray@users.noreply.github.com>
2025-05-12 20:43:04 +0200

    Create user account

--- server/main.js
+++ server/main.js
@@ -1,10 +1,21 @@
 import { Meteor } from "meteor/meteor";
 import { TasksCollection } from "/imports/api/TasksCollection";
+import { Accounts } from "meteor/accounts-base";
 
 const insertTask = async (taskText) =>
   TasksCollection.insertAsync({ text: taskText });
 
+const SEED_USERNAME = "meteorite";
+const SEED_PASSWORD = "password";
+
 Meteor.startup(async () => {
+  if (!(await Accounts.findUserByUsername(SEED_USERNAME))) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
   if ((await TasksCollection.find().countAsync()) === 0) {
     [
       "First Task",
